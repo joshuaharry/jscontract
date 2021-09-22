ef995f0f52f0e0ad42a8633f9ba136ce4dbd3d85
joshuaharry <84721070+joshuaharry@users.noreply.github.com>
2021-09-22 16:50:59 -0500

    Vendor

--- workspaces/compiler/dist/compiler.d.ts
+++ workspaces/compiler/dist/compiler.d.ts
@@ -1,11 +1,16 @@
+import { ParserPlugin } from "@babel/parser";
 interface GraphNode {
-  name: string;
-  dependencies: string[];
-  isRecursive: boolean;
-  [otherProperty: string]: any;
+    name: string;
+    dependencies: string[];
+    isRecursive: boolean;
+    [otherProperty: string]: any;
 }
 declare type Graph = Record<string, GraphNode>;
 export declare const markGraphNodes: (graph: Graph) => GraphNode[];
 export declare const ORIGINAL_MODULE_FILE = "./__ORIGINAL_UNTYPED_MODULE__.js";
-declare const compileContracts: () => string;
+interface CompilerOptions {
+    fileName: string;
+    language: ParserPlugin;
+}
+declare const compileContracts: (options?: CompilerOptions) => string;
 export default compileContracts;
