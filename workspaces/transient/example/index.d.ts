546b029f3b1aaade971c17814e624d2fc153eeb7
Joshua Hoeflich <joshuaharry411@icloud.com>
2022-02-02 11:47:19 -0600

    Prototyping transient

---
+++ workspaces/transient/example/index.d.ts
@@ -0,0 +1,4 @@
+export const EXAMPLE: string;
+export function myFunction(x: number): string;
+declare const anotherExample: (x: string) => number;
+export default anotherExample;
---
+++ workspaces/transient/example/index.js
@@ -0,0 +1,7 @@
+const EXAMPLE = 'This is an example';
+const anotherExample = 'This is another example';
+
+module.exports = {
+  EXAMPLE,
+  anotherExample,
+}
